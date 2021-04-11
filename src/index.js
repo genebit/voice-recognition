9cf3ce7e6bada27b95650db0681ec59f75628048
genebit <genebitara@gmail.com>
2021-04-11 23:10:39 +0800

    Added style.css, moved index.html outside src

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-var api_url = "https://speech.googleapis.com/v1p1beta1/speech:recognize";
 
-fetch(api_url)
-     .then((resp) => resp.json())
-     .then(function (data) {
-          let authors = data.results;
-          return authors.map(function (author) {
-               let li = createNode('li');
-               let img = createNode('img');
-               let span = createNode('span');
-               img.src = author.picture.medium;
-               span.innerHTML = `${author.name.first} ${author.name.last}`;
-               append(li, img);
-               append(li, span);
-               append(ul, li);
-          })
-     })
-     .catch(function (error) {
-          console.log(error);
-     });
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.continuous = true;
